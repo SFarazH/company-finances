5fd5d64c11c8052e25f30491e651b26f8c48e3c0
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-05-24 19:01:33 +0530

    fix ui error

--- src/pages/Companies.jsx
+++ src/pages/Companies.jsx
@@ -30,25 +30,28 @@ export default function Companies() {
 
   return (
     <>
-      {showLoader ? <Spinner/> : <div className="px-8">
-        <p className="text-3xl font-semibold my-4 ">Companies</p>
-        {clientName.map((client) => (
-          <div key={client._id} className="my-2">
-            <div
-              className="border p-3 rounded-full text-xl cursor-pointer flex justify-between"
-              onClick={() => toggleAccordion(client._id)}
-            >
-              {client.companyName}
-              <p>fff</p>
-            </div>
-            {expandedCompany === client._id && (
-              <div className="px-4 py-2">
-                <CompanyCard {...client} key={client._id} />
+      {showLoader ? (
+        <Spinner />
+      ) : (
+        <div className="px-8">
+          <p className="text-3xl font-semibold my-4 ">Companies</p>
+          {clientName.map((client) => (
+            <div key={client._id} className="my-2">
+              <div
+                className="border p-3 rounded-full text-xl cursor-pointer flex justify-between"
+                onClick={() => toggleAccordion(client._id)}
+              >
+                {client.companyName}
               </div>
-            )}
-          </div>
-        ))}
-      </div>}
+              {expandedCompany === client._id && (
+                <div className="px-4 py-2 transition duration-1000">
+                  <CompanyCard {...client} key={client._id} />
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 }
