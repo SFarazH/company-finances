a7350b939fd845f2f23363871431571406a7bde8
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-06-01 21:24:48 +0530

    fix expense form bug

--- src/components/forms/ExpenseForm.jsx
+++ src/components/forms/ExpenseForm.jsx
@@ -5,6 +5,8 @@ import { useForm } from "react-hook-form";
 const ExpenseForm = ({ setIsForm }) => {
   const [projectNames, setProjectNames] = useState([]);
   const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
   const {
     register,
     handleSubmit,
@@ -19,12 +21,14 @@ const ExpenseForm = ({ setIsForm }) => {
 
   const getProjectsName = async () => {
     const config = {
-      url: "http://localhost:4000/project/names",
+      url: "http://localhost:4000/purchase/all",
       method: "get",
     };
     axios(config)
       .then((res) => setProjectNames(res.data))
-      .catch((e) => console.log(e));
+      .catch((e) => {
+        console.log(e);
+      });
   };
   useEffect(() => {
     getProjectsName();
@@ -39,15 +43,19 @@ const ExpenseForm = ({ setIsForm }) => {
     axios(config)
       .then((res) => {
         setSuccess(true);
+        setError(false);
         setTimeout(() => {
           setIsForm(false);
         }, 1500);
       })
-      .catch((e) => console.error(e));
+      .catch((e) => {
+        setErrorMsg(e.response.data.error);
+        setError(true);
+      });
   };
 
   const expenseCategories = [
-    { name: "Project", value: "project", id: 1 },
+    { name: "Project Purchase", value: "project", id: 1 },
     { name: "Salary", value: "salary", id: 2 },
     { name: "Utilities", value: "utilities", id: 3 },
     { name: "Snacks", value: "snacks", id: 4 },
@@ -79,14 +87,14 @@ const ExpenseForm = ({ setIsForm }) => {
 
           {watch("expenseCategory") === "project" ? (
             <div className="mt-4">
-              <label className="block text-gray-700">Project Name</label>
+              <label className="block text-gray-700">Product Name</label>
               <select
                 {...register("projectPurchaseId", { required: true })}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
               >
                 <option value="">Select a company</option>
-                {projectNames.map((project) => (
-                  <option value={project._id}>{project.projectName}</option>
+                {projectNames.map((product) => (
+                  <option value={product._id}>{product.productName}</option>
                 ))}
               </select>
               {errors.projectPurchaseId && (
@@ -168,6 +176,12 @@ const ExpenseForm = ({ setIsForm }) => {
           {success && (
             <p className="text-green-500 font-semibold font-lg text-center pb-4">
               Expense added successfully!
+            </p>
+          )}
+
+          {error && (
+            <p className="text-red-500 font-semibold font-lg text-center pb-4">
+              {errorMsg}
             </p>
           )}
 
