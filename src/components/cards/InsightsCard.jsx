da1ca1b5b5ada4f8987c6064ca978f2f5ed56053
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-07-05 18:55:18 +0530

    update KPI

--- src/components/cards/InsightsCard.jsx
+++ src/components/cards/InsightsCard.jsx
@@ -4,12 +4,14 @@ export default function InsightCard(props) {
     <>
       <div className="card p-2 border border-white my-4 rounded-xl">
         <div className="head flex justify-between items-center">
-          <div><p className="text-md font-semibold uppercase">{props.category}</p>
-          <p className="text-2xl font-bold">{props.value}</p></div>
-          {/* <FaRupeeSign size={30}/> */}
+          <div>
+            <p className="text-md font-semibold uppercase">{props.category}</p>
+            <p className="text-2xl font-bold">
+              {props.value ? props.value : 0}
+            </p>
+          </div>
           <div className="">{props.icon}</div>
         </div>
-        
       </div>
     </>
   );
